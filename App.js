170bc96b8574fa0c14a00a051cffa6c8261a7a01
THAMITHA S <THAMITHA.S@ibm.com>
2025-03-26 19:50:20 +0530

    added functional component

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-const heading = React.createElement("h1",{ id: "heading"},"Hello World from React!")
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+import React from "react";
+import ReactDom from "react-dom/client";
+
+const heading = <h1>Namaste React 🚀</h1>
+const HeadingComponent = () => (<div>
+        <h1 id= 'heading'>Namaste React thamitha 🚀</h1>
+    </div>)
+const root = ReactDom.createRoot(document.getElementById('root'))
+root.render(<HeadingComponent />)
 
 
