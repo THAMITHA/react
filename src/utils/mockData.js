1036ee10a46c5b4f9839451462af457efcb4272f
THAMITHA S <THAMITHA.S@ibm.com>
2025-04-02 16:27:36 +0530

    added components

---
+++ src/utils/mockData.js
@@ -0,0 +1,703 @@
+const resList = [
+    {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+    "id": "1003414",
+    "name": "Pizza Hut",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/5/dc74a4b7-8d63-4eb7-91c9-048973c2cc99_1003414.jpg",
+    "locality": "Chhindwara",
+    "areaName": "Parasia Road",
+    "costForTwo": "₹350 for two",
+    "cuisines": [
+    "Pizzas"
+    ],
+    "avgRating": 4.1,
+    "parentId": "721",
+    "avgRatingString": "4.1",
+    "totalRatingsString": "51",
+    "sla": {
+    "deliveryTime": 56,
+    "lastMileTravel": 13.8,
+    "serviceability": "SERVICEABLE",
+    "slaString": "55-60 mins",
+    "lastMileTravelString": "13.8 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-31 23:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "ITEMS",
+    "subHeader": "AT ₹64"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "isNewlyOnboarded": true,
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-fac5fc31-9106-4bd5-abc2-a61cba2a08e4"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/chhindwara/pizza-hut-parasia-road-rest1003414",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+    "id": "769851",
+    "name": "Healthy Bites",
+    "cloudinaryImageId": "4dfbe240cf1a2be01e6f2805c75c4279",
+    "locality": "Collectrate Road",
+    "areaName": "Chhindwara City",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "Salads"
+    ],
+    "veg": true,
+    "parentId": "3143",
+    "avgRatingString": "--",
+    "sla": {
+    "deliveryTime": 54,
+    "lastMileTravel": 11.3,
+    "serviceability": "SERVICEABLE",
+    "slaString": "50-55 mins",
+    "lastMileTravelString": "11.3 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-31 22:00:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "v1695133679/badges/Pure_Veg111.png",
+    "description": "pureveg"
+    }
+    ]
+    },
+    "isOpen": true,
+    "aggregatedDiscountInfoV2": {},
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "pureveg",
+    "imageId": "v1695133679/badges/Pure_Veg111.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "5.0",
+    "ratingCount": "<3"
+    },
+    "source": "GOOGLE",
+    "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_EXTERNAL"
+    },
+    "analytics": {
+    "context": "seo-data-fac5fc31-9106-4bd5-abc2-a61cba2a08e4"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/chhindwara/healthy-bites-collectrate-road-chhindwara-city-rest769851",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+    "id": "234875",
+    "name": "Adil Hotel",
+    "cloudinaryImageId": "gp1ityra6utvzqn6ghnv",
+    "locality": "Rautha Wada",
+    "areaName": "Chhindwara Locality",
+    "costForTwo": "₹150 for two",
+    "cuisines": [
+    "North Indian",
+    "Biryani",
+    "Tandoor"
+    ],
+    "avgRating": 4.3,
+    "parentId": "27123",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "1.2K+",
+    "sla": {
+    "deliveryTime": 47,
+    "lastMileTravel": 11.5,
+    "serviceability": "SERVICEABLE",
+    "slaString": "45-50 mins",
+    "lastMileTravelString": "11.5 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-04-01 00:00:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "ITEMS",
+    "subHeader": "AT ₹129"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-fac5fc31-9106-4bd5-abc2-a61cba2a08e4"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/chhindwara/adil-hotel-rautha-wada-chhindwara-locality-rest234875",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+    "id": "150602",
+    "name": "Raimens Cafe 79",
+    "cloudinaryImageId": "eohdm5sdnq6e2u46xeux",
+    "locality": "Teacher's Colony",
+    "areaName": "VIP Road",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "Bakery",
+    "Pizzas",
+    "Snacks",
+    "Beverages"
+    ],
+    "avgRating": 4.4,
+    "veg": true,
+    "parentId": "164782",
+    "avgRatingString": "4.4",
+    "totalRatingsString": "4.2K+",
+    "sla": {
+    "deliveryTime": 54,
+    "lastMileTravel": 12.5,
+    "serviceability": "SERVICEABLE",
+    "slaString": "50-55 mins",
+    "lastMileTravelString": "12.5 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-31 22:30:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "v1695133679/badges/Pure_Veg111.png",
+    "description": "pureveg"
+    }
+    ]
+    },
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "pureveg",
+    "imageId": "v1695133679/badges/Pure_Veg111.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "ITEMS",
+    "subHeader": "AT ₹49"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-fac5fc31-9106-4bd5-abc2-a61cba2a08e4"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/chhindwara/raimens-cafe-79-teachers-colony-vip-road-rest150602",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+    "id": "898041",
+    "name": "Thikana",
+    "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/5/26/78517964-20dc-4e37-92ce-8990deb14ab6_898041.jpg",
+    "locality": "RANGLA PUNJAB PARASIA ROAD",
+    "areaName": " ADARSH NAGAR CHHINDWARA",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "Indian"
+    ],
+    "avgRating": 4.2,
+    "parentId": "215738",
+    "avgRatingString": "4.2",
+    "totalRatingsString": "43",
+    "sla": {
+    "deliveryTime": 53,
+    "lastMileTravel": 13.6,
+    "serviceability": "SERVICEABLE",
+    "slaString": "50-55 mins",
+    "lastMileTravelString": "13.6 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-31 23:45:00",
+    "opened": true
+    },
+    "badges": {},
+    "isOpen": true,
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {},
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "aggregatedDiscountInfoV3": {
+    "header": "₹50 OFF",
+    "subHeader": "ABOVE ₹199",
+    "discountTag": "FLAT DEAL"
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-fac5fc31-9106-4bd5-abc2-a61cba2a08e4"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/chhindwara/thikana-rangla-punjab-parasia-road-adarsh-nagar-chhindwara-rest898041",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+    "id": "151649",
+    "name": "Hotel Sai Nath & Sai Restaurant",
+    "cloudinaryImageId": "vkhcohhmqfczycw9vsar",
+    "locality": "railway station",
+    "areaName": "Chhindwara Locality",
+    "costForTwo": "₹200 for two",
+    "cuisines": [
+    "North Indian",
+    "South Indian",
+    "Chinese",
+    "Beverages",
+    "Fast Food",
+    "Desserts"
+    ],
+    "avgRating": 4.3,
+    "veg": true,
+    "parentId": "101802",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "980",
+    "sla": {
+    "deliveryTime": 49,
+    "lastMileTravel": 10.3,
+    "serviceability": "SERVICEABLE",
+    "slaString": "45-50 mins",
+    "lastMileTravelString": "10.3 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-31 22:30:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "v1695133679/badges/Pure_Veg111.png",
+    "description": "pureveg"
+    }
+    ]
+    },
+    "isOpen": true,
+    "aggregatedDiscountInfoV2": {},
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "pureveg",
+    "imageId": "v1695133679/badges/Pure_Veg111.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-fac5fc31-9106-4bd5-abc2-a61cba2a08e4"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/chhindwara/hotel-sai-nath-and-sai-restaurant-railway-station-chhindwara-locality-rest151649",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+    "id": "151518",
+    "name": "Bakery World",
+    "cloudinaryImageId": "mt2aggiscfl3yviatwng",
+    "locality": "Parasia Road",
+    "areaName": "Parasia Road",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "Bakery",
+    "Ice Cream",
+    "Snacks",
+    "Beverages"
+    ],
+    "avgRating": 4.3,
+    "veg": true,
+    "parentId": "40363",
+    "avgRatingString": "4.3",
+    "totalRatingsString": "261",
+    "sla": {
+    "deliveryTime": 44,
+    "lastMileTravel": 11.7,
+    "serviceability": "SERVICEABLE",
+    "slaString": "40-45 mins",
+    "lastMileTravelString": "11.7 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-31 22:30:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "v1695133679/badges/Pure_Veg111.png",
+    "description": "pureveg"
+    }
+    ]
+    },
+    "isOpen": true,
+    "aggregatedDiscountInfoV2": {},
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "pureveg",
+    "imageId": "v1695133679/badges/Pure_Veg111.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-fac5fc31-9106-4bd5-abc2-a61cba2a08e4"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/chhindwara/bakery-world-parasia-road-rest151518",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+    "id": "151661",
+    "name": "Bharat Mewad Ice Cream",
+    "cloudinaryImageId": "auczcmkdvvxzt2mswvcc",
+    "locality": "Teacher's Colony",
+    "areaName": "Chhindwara Locality",
+    "costForTwo": "₹250 for two",
+    "cuisines": [
+    "Ice Cream",
+    "Desserts",
+    "Beverages"
+    ],
+    "avgRating": 4.5,
+    "veg": true,
+    "parentId": "44378",
+    "avgRatingString": "4.5",
+    "totalRatingsString": "461",
+    "sla": {
+    "deliveryTime": 41,
+    "lastMileTravel": 10.9,
+    "serviceability": "SERVICEABLE",
+    "slaString": "40-45 mins",
+    "lastMileTravelString": "10.9 km",
+    "iconType": "ICON_TYPE_EMPTY"
+    },
+    "availability": {
+    "nextCloseTime": "2025-03-31 23:00:00",
+    "opened": true
+    },
+    "badges": {
+    "imageBadges": [
+    {
+    "imageId": "v1695133679/badges/Pure_Veg111.png",
+    "description": "pureveg"
+    }
+    ]
+    },
+    "isOpen": true,
+    "aggregatedDiscountInfoV2": {},
+    "type": "F",
+    "badgesV2": {
+    "entityBadges": {
+    "imageBased": {
+    "badgeObject": [
+    {
+    "attributes": {
+    "description": "pureveg",
+    "imageId": "v1695133679/badges/Pure_Veg111.png"
+    }
+    }
+    ]
+    },
+    "textBased": {},
+    "textExtendedBadges": {}
+    }
+    },
+    "orderabilityCommunication": {
+    "title": {},
+    "subTitle": {},
+    "message": {},
+    "customIcon": {}
+    },
+    "differentiatedUi": {
+    "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+    "differentiatedUiMediaDetails": {
+    "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+    "lottie": {},
+    "video": {}
+    }
+    },
+    "reviewsSummary": {},
+    "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    "restaurantOfferPresentationInfo": {},
+    "externalRatings": {
+    "aggregatedRating": {
+    "rating": "--"
+    }
+    },
+    "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+    "context": "seo-data-fac5fc31-9106-4bd5-abc2-a61cba2a08e4"
+    },
+    "cta": {
+    "link": "https://www.swiggy.com/city/chhindwara/bharat-mewad-ice-cream-teachers-colony-chhindwara-locality-rest151661",
+    "text": "RESTAURANT_MENU",
+    "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    }
+    ]
+
+    export default resList
