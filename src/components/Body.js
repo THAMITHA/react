d1a06446b1a57ac42681fa257c1fa7dfe3092907
THAMITHA S <THAMITHA.S@ibm.com>
2025-04-06 06:04:15 +0530

    added ternary operator

--- src/components/Body.js
+++ src/components/Body.js
@@ -18,10 +18,8 @@ const Body = () => {
         setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements.infoWithStyle.restaurants)
 
     }
-    if(listOfRestaurants.length === 0){
-        return <Shimmer />
-    }
-    return (
+    
+    return listOfRestaurants.length === 0 ? <Shimmer /> : (
      <div className="body">
          <div className="filter">
             <button className="filter-btn" onClick={()=>{filteredList = listOfRestaurants.filter((res)=> res.info.avgRatingString>4)
