2a79db810f25193db185b26fe93aa5c0f9f43523
THAMITHA S <THAMITHA.S@ibm.com>
2025-04-13 09:19:29 +0530

    update dynamic route

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,29 +1,30 @@
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router";
+import { MENU_API } from "../utils/constants";
 
 const RestaurantMenu = () => {
     const [resInfo, setResInfo] = useState(null)
+    const { resId } = useParams()
     useEffect(()=>{
         fetchMenu();
     }, [])
 
     const fetchMenu = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9564868&lng=77.6420348&restaurantId=661411&catalog_qa=undefined&submitAction=ENTER")
+        const data = await fetch(MENU_API+resId)
         const json = await data.json()
         console.log('pleasecheck', json)
         setResInfo(json.data)
     }
     if(resInfo == null) return  <Shimmer/>
-    const { name, cuisines, costForTwoMessage} = resInfo.cards[2].card.card.info 
+    const { name, cuisines, costForTwoMessage} = resInfo.cards[2].card.card.info
+    const { itemCards } = resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[3].card.card
     return ( 
         <div className="menu">
             <h1>{name}</h1>
             <p>{cuisines.join(', ')} - {costForTwoMessage}</p>
-            <h3>{resInfo.cards[4].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards[0].card.info.name}</h3>
-            <ul>
-                <li>Biryani</li>
-                <li>Burgers</li>
-                <li>Diet Coke</li>
+            <h2>Menu</h2>
+            <ul>{itemCards.map(item => <li key={item.card.info.id}>{item.card.info.name} - {"Rs."}{item.card.info.price/100}</li>)}
             </ul>
         </div>
     )
