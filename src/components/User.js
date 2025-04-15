41abc15e85cc0851be14e7a8796a0935d4868118
THAMITHA S <THAMITHA.S@ibm.com>
2025-04-15 21:18:57 +0530

    add class-component

---
+++ src/components/User.js
@@ -0,0 +1,9 @@
+const User = () => {
+    return <div className="user-card">
+        <h2>Name: Thamitha</h2>
+        <h3>Location: Bangalore</h3>
+        <h4>Contact: @thamitha</h4>
+    </div>
+}
+
+export default User
---
+++ src/components/UserClass.js
@@ -0,0 +1,12 @@
+import React from "react"
+class UserClass extends React.Component{
+    render(){
+        return <div className="user-card">
+            <h2>Name: Thamitha</h2>
+            <h3>Location: Bangalore</h3>
+            <h4>Contact: @thamitha</h4>
+        </div>
+    }
+}
+
+export default UserClass
