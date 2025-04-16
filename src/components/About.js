03155b102b0bbc69c5a3a3f4f967a1ab77f1c12f
THAMITHA S <THAMITHA.S@ibm.com>
2025-04-16 05:50:05 +0530

    adding functional component

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,12 @@
 import User from "./user";
 import UserClass from "./UserClass";
 
-const About = () => {
+const About = (props) => {
     return (
         <div>
             <h1>About</h1>
             <h2>This is Namaste web Series</h2>
-            <User/>
+            <User name={"Thamitha(functional)"}/>
             <UserClass/>
          </div>        
     )
