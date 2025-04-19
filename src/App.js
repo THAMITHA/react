3eea98ec5d15dca00c959d402eae8bfe7b5f7d5d
THAMITHA S <THAMITHA.S@ibm.com>
2025-04-19 16:18:45 +0530

    addded lazy loading

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react"
+import React, { lazy, Suspense } from "react"
 import ReactDOM from "react-dom/client";
 import { Header } from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-
+const About = lazy(()=>import("./components/About"))
+const Grocery = lazy(()=>import("./components/Grocery"))
 const AppLayout = () => {
     return(
         <div className="app">
@@ -28,7 +28,7 @@ const appRouter = createBrowserRouter([
             },
             {
                 path: "/about",
-                element: <About/>,
+                element: <Suspense fallback={<h1>Loading...</h1>}><About/></Suspense>,
             },
             {
                 path: "/contact",
@@ -37,6 +37,10 @@ const appRouter = createBrowserRouter([
             {
                 path: "/restaurants/:resId",
                 element: <RestaurantMenu/>
+            },
+            {
+                path: "/Grocery",
+                element: <Suspense fallback={<h1>Loading...</h1>}><Grocery/></Suspense>
             }
         ],
         errorElement: <Error/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,11 @@ import { LOGO_URL } from "../utils/constants"
 import {useEffect, useState} from 'react'
 import { Link } from "react-router-dom"
 import About from "./About"
+import useOnlineStatus from "../utils/useOnlineStatus"
+
 export const Header = ()=>{
     const [btnNameReact, setBtnNameReact] = useState("Login")
+    const onlineStatus = useOnlineStatus()
     console.log("header render")
     useEffect(()=>{
         console.log("useEffect called")
@@ -16,9 +19,11 @@ export const Header = ()=>{
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
+                    <li>online status: {onlineStatus? "✅":"🔴"}</li>
+                    <li><Link to={"/"}>Home</Link></li>
                     <li><Link to={"/about"}>About Us</Link></li>
                     <li><Link to={"/contact"}>Contact Us</Link></li>
+                    <li><Link to={"/grocery"}>Grocery</Link></li>
                     <li><Link to={"/cart"}>Cart</Link></li>
                     <button className="login" onClick={()=>{
                         btnNameReact==="Login"? setBtnNameReact("Logout"): setBtnNameReact("Login")
