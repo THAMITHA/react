aba022f2398801cfff9ce5e194d8beb68581ff97
THAMITHA S <THAMITHA.S@ibm.com>
2025-04-21 23:42:24 +0530

    update style using css

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,18 +13,18 @@ export const Header = ()=>{
     },[btnNameReact])
 
     return(
-        <div className="flex">
+        <div className="flex justify-between bg-pink-100 shadow-lg">
             <div className="w-56">
                 <img className="logo" src={LOGO_URL}/>
             </div>
-            <div className="nav-items">
-                <ul className="flex">
-                    <li>online status: {onlineStatus? "✅":"🔴"}</li>
-                    <li><Link to={"/"}>Home</Link></li>
-                    <li><Link to={"/about"}>About Us</Link></li>
-                    <li><Link to={"/contact"}>Contact Us</Link></li>
-                    <li><Link to={"/grocery"}>Grocery</Link></li>
-                    <li><Link to={"/cart"}>Cart</Link></li>
+            <div className="flex items-center">
+                <ul className="flex p-4 m-4">
+                    <li className="px-4">online status: {onlineStatus? "✅":"🔴"}</li>
+                    <li className="px-4"><Link to={"/"}>Home</Link></li>
+                    <li className="px-4"><Link to={"/about"}>About Us</Link></li>
+                    <li className="px-4"><Link to={"/contact"}>Contact Us</Link></li>
+                    <li className="px-4"><Link to={"/grocery"}>Grocery</Link></li>
+                    <li className="px-4"><Link to={"/cart"}>Cart</Link></li>
                     <button className="login" onClick={()=>{
                         btnNameReact==="Login"? setBtnNameReact("Logout"): setBtnNameReact("Login")
                     }}>{btnNameReact}</button>
