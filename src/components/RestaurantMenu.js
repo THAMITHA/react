01bfbb356dfaf2ea7dad4319325f385360401b8d
THAMITHA S <THAMITHA.S@ibm.com>
2025-04-26 23:27:33 +0530

    update add button

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -12,6 +12,7 @@ const RestaurantMenu = () => {
     const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card
     console.log('itemCard', resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards)
     const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(c => c.card?.["card"]?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory")
+    console.log('categories', resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards)
     return ( 
         <div className="text-center">
             <h1 className="font-bold my-6 text-2xl">{name}</h1>
