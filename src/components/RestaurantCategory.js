042c67eff5b897c129281bc60cca3fc17387f434
THAMITHA S <THAMITHA.S@ibm.com>
2025-04-26 23:40:39 +0530

    add toggle feature

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,15 +1,21 @@
+import { useState } from "react"
 import ItemList from "./ItemList"
 
 const RestaurantCategory = ({data}) => {
+    const [showItems, setShowItems] = useState(false)
     console.log('data', data.itemCards)
+    const handleClick = () =>{
+        setShowItems(!showItems)
+    }
     return( 
     <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 flex ">
-        <div className="justify-between">
+        <div className="flex justify-between cursor-pointer" onClick={handleClick}>
             {/* header */}
             <span className="font-bold text-lg">{data.title} ({data.itemCards.length})</span>
             <span>⬇️</span>
             {/* Accordion Body */}
-            <ItemList items = {data.itemCards}/>
+
+            {showItems && <ItemList items = {data.itemCards}/>}
         </div>
     </div>
     )
