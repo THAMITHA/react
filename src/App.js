353ed66d58832678599a306a3ffee290d59d68d4
THAMITHA S <THAMITHA.S@ibm.com>
2025-04-27 01:16:43 +0530

    updated user-context

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react"
+import React, { lazy, Suspense, useEffect, useState } from "react"
 import ReactDOM from "react-dom/client";
 import { Header } from "./components/Header";
 import Body from "./components/Body";
@@ -6,7 +6,9 @@ import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import UserContext from "./utils/UserContext";
 const About = lazy(()=>import("./components/About"))
+
 
 //Chunking
 //Code Splitting
@@ -17,11 +19,22 @@ const About = lazy(()=>import("./components/About"))
 
 const Grocery = lazy(()=>import("./components/Grocery"))
 const AppLayout = () => {
+    const [userName, setUserName] = useState()
+    
+    //authentication
+    useEffect(()=>{
+        // make an api call and send username and password
+        const data = {name: "Thamitha"}
+        setUserName(data.name)
+    }, 
+    [])
     return(
-        <div className="app">
-            <Header/>
-            <Outlet />    
-        </div>
+        <UserContext.Provider value={{loggedInUser: userName}}>
+            <div className="app">
+                <Header/>
+                <Outlet />    
+            </div>
+        </UserContext.Provider>    
     )
 }
 
