f0cc156db07b0b80e032487843ef959beec6ac7a
THAMITHA S <THAMITHA.S@ibm.com>
2025-04-27 18:14:48 +0530

    update categoryMenu

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -7,12 +7,10 @@ const RestaurantCategory = ({data, showItems, setShowIndex, dummy}) => {
         setShowIndex()
     }
     return( 
-    <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 flex ">
-        <div className="flex justify-between cursor-pointer" onClick={handleClick}>
-            {/* header */}
+    <div>
+        <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4 flex justify-between cursor-pointer" onClick={handleClick}>
             <span className="font-bold text-lg">{data.title} ({data.itemCards.length})</span>
             <span>⬇️</span>
-            {/* Accordion Body */}
 
             {showItems && <ItemList items = {data.itemCards} dummy = {dummy}/>}
         </div>
