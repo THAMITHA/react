9a7c17e6b661845403eb1459864e28d2b339f8ca
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-02 04:34:56 +0530

    subscribe to store using selector

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,17 @@ import { Link } from "react-router-dom"
 import About from "./About"
 import useOnlineStatus from "../utils/useOnlineStatus"
 import UserContext from "../utils/UserContext"
+import { useSelector } from "react-redux"
 
 export const Header = ()=>{
     const [btnNameReact, setBtnNameReact] = useState("Login")
     const onlineStatus = useOnlineStatus()
     const {loggedInUser} = useContext(UserContext)
     console.log("header render")
+
+    //Subscribing to the store using a Selector
+    const cartItems = useSelector((store)=> store.cart.items)
+
     return(
         <div className="flex justify-between bg-pink-100 shadow-lg sm:bg-yellow-50 lg:bg-green-50">
             <div className="w-56">
@@ -22,7 +27,8 @@ export const Header = ()=>{
                     <li className="px-4"><Link to={"/about"}>About Us</Link></li>
                     <li className="px-4"><Link to={"/contact"}>Contact Us</Link></li>
                     <li className="px-4"><Link to={"/grocery"}>Grocery</Link></li>
-                    <li className="px-4 font-bold text-xl"><Link to={"/cart"}>Cart (0 items)</Link></li>
+                    <li className="px-4 font-bold text-xl"><Link to={"/cart"}>
+                        Cart - ({cartItems.length} items)</Link></li>
                     <button className="login" onClick={()=>{
                         btnNameReact==="Login"? setBtnNameReact("Logout"): setBtnNameReact("Login")
                     }}>{btnNameReact}</button>
