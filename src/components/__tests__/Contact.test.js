f0d637c1e1cf13ebf40936489e5a7fb57bea8d1c
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-03 15:00:28 +0530

    update test to it

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -3,31 +3,31 @@ import Contact from '../Contact';
 import '@testing-library/jest-dom';
 
 describe("Contact Us Page Test Case", () => {
-    test("should load heading inside contact us component", ()=> {
+    it("should load heading inside contact us component", ()=> {
         render(<Contact/>)
         const heading = screen.getByRole("heading")
         expect(heading).toBeInTheDocument();
     })
     
-    test("should load button button inside contact us component", ()=> {
+    it("should load button button inside contact us component", ()=> {
         render(<Contact/>)
         const button = screen.getByRole("button")
         expect(button).toBeInTheDocument();
     })
     
-    test("should load Submit test inside contact us component", ()=> {
+    it("should load Submit test inside contact us component", ()=> {
         render(<Contact/>)
         const button = screen.getByText("Submit")
         expect(button).toBeInTheDocument();
     })
     
-    test("should load input name inside contact us component", ()=> {
+    it("should load input name inside contact us component", ()=> {
         render(<Contact/>)
         const inputName = screen.getByPlaceholderText("name")
         expect(inputName).toBeInTheDocument();
     })
     
-    test("should load 2 input boxed on the Contact component", ()=>{
+    it("should load 2 input boxed on the Contact component", ()=>{
         render(<Contact/>)
         const inputBoxes = screen.getAllByRole("textbox"); //multiple items present
         expect(inputBoxes.length).not.toBe(4)
