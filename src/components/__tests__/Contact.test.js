ac23826309faf05518d6090c5dbca43933125777
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-03 14:39:51 +0530

    added testcase

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,14 +2,20 @@ import {render, screen} from '@testing-library/react';
 import Contact from '../Contact';
 import '@testing-library/jest-dom';
 
-test("should load contact us component", ()=> {
+test("should load heading inside contact us component", ()=> {
     render(<Contact/>)
     const heading = screen.getByRole("heading")
     expect(heading).toBeInTheDocument();
 })
 
-test("should load contact us component", ()=> {
+test("should load button button inside contact us component", ()=> {
     render(<Contact/>)
     const button = screen.getByRole("button")
+    expect(button).toBeInTheDocument();
+})
+
+test("should load Submit test inside contact us component", ()=> {
+    render(<Contact/>)
+    const button = screen.getByText("Submit")
     expect(button).toBeInTheDocument();
 })
