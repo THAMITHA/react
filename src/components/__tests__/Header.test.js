364ec7eaddfc8c31bf8e7a045fac1a544760e7e7
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-04 12:24:14 +0530

    added testcase

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,10 +1,11 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import appStore from "../../utils/appStore"
 import { Header } from "../Header" 
 import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom"
 
-it("Should load Header Component with a login button", () => {
+it("Should render Header Component with a login button", () => {
     render(
         <BrowserRouter>
             <Provider store={appStore}>
@@ -12,4 +13,6 @@ it("Should load Header Component with a login button", () => {
             </Provider>
         </BrowserRouter>
     )
+    const loginButton = screen.getByRole("button")
+    expect(loginButton).toBeInTheDocument();
 })
