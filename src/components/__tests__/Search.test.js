9cb0f3424c81aa7bd970d59c41dc515e42d53a56
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-05 10:10:47 +0530

    added body testcomponent

---
+++ src/components/__tests__/Search.test.js
@@ -0,0 +1,14 @@
+import { render } from "@testing-library/react";
+import Body from "../Body";
+import MOCK_DATA from "../mocks/mockResListData.json"
+
+global.fetch = jest.fn(()=>{
+    return Promise.resolve({
+        json: () => {
+            return Promise.resolve(MOCK_DATA);
+        }
+    })
+});
+it("should render the Body Component with Search", () => {
+    render(<Body/>)
+});
