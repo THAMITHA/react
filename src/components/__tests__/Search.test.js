f34131878c8bbcdc976970710964f42321eec92f
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-05 10:31:11 +0530

    update browser-router

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -1,6 +1,8 @@
 import { render } from "@testing-library/react";
 import Body from "../Body";
+import { act } from "@testing-library/react";
 import MOCK_DATA from "../mocks/mockResListData.json"
+import { BrowserRouter } from "react-router-dom";
 
 global.fetch = jest.fn(()=>{
     return Promise.resolve({
@@ -9,6 +11,11 @@ global.fetch = jest.fn(()=>{
         }
     })
 });
-it("should render the Body Component with Search", () => {
-    render(<Body/>)
+it("should render the Body Component with Search", async() => {
+    await act(async () => 
+    render(
+        <BrowserRouter>
+            <Body/>
+        </BrowserRouter>
+    ))
 });
