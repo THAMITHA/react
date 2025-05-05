b6dcc9a3e28370a84136dc6357fc9f86cc26eec6
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-06 00:53:08 +0530

    updated header

--- src/components/__tests__/Cart.test.js
+++ src/components/__tests__/Cart.test.js
@@ -1,9 +1,11 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { act } from "react";
 import RestaurantMenu from "../RestaurantMenu";
+import { Header } from "../Header";
 import { Provider } from "react-redux";
 import MOCK_DATA_NAME from "../mocks/mockResMenu.json"
 import appStore from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
 
 global.fetch = jest.fn(() => 
     Promise.resolve({
@@ -12,14 +14,20 @@ global.fetch = jest.fn(() =>
 )
 
 it("Should load Restaurant menu Component", async () =>{
-    await act(async () => render(
-        <Provider store={appStore}>
+    await act(async () => 
+        render(
+        <BrowserRouter>
+            <Provider store={appStore}>
+            <Header/>
             <RestaurantMenu/>
-        </Provider>));
+        </Provider>
+        </BrowserRouter>
+        )
+    );
     const accordionHeader = screen.getByText("Drinks (9)");
     fireEvent.click(accordionHeader);
     expect(screen.getAllByTestId('foodItems').length).toBe(9);
-    const addBtn = screen.getAllByRole("button",{name: "Add +"});
-    console.log(addBtn)
+    const addBtns = screen.getAllByRole("button",{name: "Add +"});
+    fireEvent.click(addBtns[0])
     // expect(accordionHeader).toBe("Drinks (9)")
 })
