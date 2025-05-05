7b6715f789a0a9d03880225f0918b7345cf30cdf
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-05 11:13:40 +0530

    updated search testcase

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -19,9 +19,14 @@ it("should render the Body Component with Search", async() => {
             <Body/>
         </BrowserRouter>
     ));
+
+    const cardsBeforeSearch = screen.getAllByTestId("resCard")
+    expect(cardsBeforeSearch.length).toBe(8)
+
     const searchBtn = screen.getByRole("button", {name: "search"});
     console.log('SearchButton', searchBtn)
     const searchInput = screen.getByTestId('searchInput')
+
     console.log('searchInput', searchInput)
     fireEvent.change(searchInput, {target: {value: "burger"}})
     fireEvent.click(searchBtn)
