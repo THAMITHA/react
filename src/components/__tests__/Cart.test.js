d973b919fa9d7a82e38aff01e09dad6baccc8787
THAMITHA S <THAMITHA.S@ibm.com>
2025-05-06 05:57:56 +0530

    added cart elements

--- src/components/__tests__/Cart.test.js
+++ src/components/__tests__/Cart.test.js
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import MOCK_DATA_NAME from "../mocks/mockResMenu.json"
 import appStore from "../../utils/appStore";
 import { BrowserRouter } from "react-router-dom";
+import Cart from '../Cart'
 import "@testing-library/jest-dom"
 
 global.fetch = jest.fn(() => 
@@ -18,9 +19,10 @@ it("Should load Restaurant menu Component", async () =>{
     await act(async () => 
         render(
         <BrowserRouter>
-            <Provider store={appStore}>
+            <Provider store={appStore}>  
             <Header/>
             <RestaurantMenu/>
+            <Cart/> 
         </Provider>
         </BrowserRouter>
         )
@@ -28,11 +30,11 @@ it("Should load Restaurant menu Component", async () =>{
     const accordionHeader = screen.getByText("Drinks (9)");
     fireEvent.click(accordionHeader);
     expect(screen.getAllByTestId('foodItems').length).toBe(9);
-    expect(screen.getByText('Cart - (0 items)')).toBeInTheDocument();
+    expect(screen.getByText("Cart - (0 items)")).toBeInTheDocument();
     const addBtns = screen.getAllByRole("button",{name: "Add +"});
     fireEvent.click(addBtns[0])
     expect(screen.getByText('Cart - (1 items)')).toBeInTheDocument();
     fireEvent.click(addBtns[1])
     expect(screen.getByText('Cart - (2 items)')).toBeInTheDocument();
-    // expect(accordionHeader).toBe("Drinks (9)")
+    expect(screen.getAllByTestId("foodItems").length).toBe(11)
 })
